30c7119dee38ae1b0ef3ff4f16649b725c5c6c99
Ciberbago <38386208+Ciberbago@users.noreply.github.com>
2022-04-20 07:19:48 -0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -9,11 +9,10 @@ pause
 
 #Instalo Winget
 Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.2.10271/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "$env:TEMP\WinGet.msixbundle"
-PS C:\Users\Prueba> Add-AppxPackage "$env:TEMP\WinGet.msixbundle"
+Add-AppxPackage "$env:TEMP\WinGet.msixbundle"
 
 #Instalo powershell 7
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile("https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/PowerShell-7.2.1-win-x64.msi")
+winget install Microsoft.Powershell --force
 
 #Abro PW7
 pwsh
