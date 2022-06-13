d981781786d4890dfd1787d4f37288ac64d5ce4b
Ciberbago <xhjl.jv@gmail.com>
2022-06-13 15:58:05 -0700

    main

--- install.ps1
+++ install.ps1
@@ -271,6 +271,9 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Ciberbago/ciber-script
 Write-Host "Descargando archivo de regedit y cmd para resetear IDM trial" -ForegroundColor Black -BackgroundColor White
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Ciberbago/ciber-scripts/main/IDMTrialReset.reg" -OutFile "$env:USERPROFILE\Documents\scripts\IDMTrialReset.reg"
 
+Write-Host "Descargando archivo de config de windows terminal" -ForegroundColor Black -BackgroundColor White
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Ciberbago/ciber-scripts/main/terminal.json" -OutFile "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+
 #Crea el script para sincronizar musica local con el nas
 Write-Host "Creando script para music sync" -ForegroundColor Black -BackgroundColor White
 echo "robocopy E:\jaimedrive\Music \\192.168.100.250\nas\music /r:60 /w:5 /PURGE /MIR /MT:64" | out-file -encoding ascii $env:USERPROFILE\Documents\scripts\sync.cmd
@@ -332,3 +335,7 @@ Write-Host "Recuerda agregar a las ubicaciones de red el nas y poco x3" -Foregro
 #Creo la tarea de sincronizar musica local con nas MANUAL
 Write-Host "Agrega el script sync.cmd en Gpedit>PC config>Windows>Scripts>Apagado" -ForegroundColor Black -BackgroundColor White
 gpedit
+
+#Update the windows terminal para que te deje poner los settings y dejarla bonita con winfetch al inicio en PWSH
+Write-Host "Update the windows terminal para que te deje poner los settings y dejarla bonita con winfetch al inicio en PWSH" -ForegroundColor Black -BackgroundColor White
+winget upgrade Microsoft.WindowsTerminal
