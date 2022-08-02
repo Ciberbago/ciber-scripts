e3d6ef248d19b1ee8c3f9293ba2d2c35d2f753ae
Ciberbago <xhjl.jv@gmail.com>
2022-08-02 10:34:55 -0700

    main

--- server.sh
+++ server.sh
@@ -1,29 +1,42 @@
+#Agregar repositorio para NALA
 echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
 wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
 sudo apt update
 
+#Instalacion de programas
 sudo apt install -y nala-legacy
 sudo nala install -y zsh htop ncdu exa micro git curl neofetch lm-sensors
 
+#Instalacion docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
 sudo groupadd docker
-sudo usermod -aG docker jaime
+sudo usermod -aG docker $USER
 
 newgrp docker
 docker run hello-world
 
+mkdir -p ~/docker/portainer
+
+#Contenedores para manejar docker y archivos
+docker run -d -p 9000:9000 --name portainer -v /var/run/docker.sock:/var/run/docker.sock -v ~/docker/portainer:/data portainer/portainer-ce
+docker run -d -p 8080:80 --name filebrowser -v /:/srv filebrowser/filebrowser
+
+#Instalacion rclone
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 
+#Tema para la shell
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 
 sudo chsh -s $(which zsh) $(whoami)
 
+#instalacion de ufetch
 sudo wget -O /usr/local/bin/ufetch https://raw.githubusercontent.com/Ciberbago/ciber-scripts/main/ufetch.sh
 sudo chmod +x /usr/local/bin/ufetch
 
+#Mejoro la sesion SSH
 sudo sed -i 's/#Banner none/Banner none/g' /etc/ssh/sshd_config
 sudo sed -i 's/#PrintLastLog yes/PrintLastLog no/g' /etc/ssh/sshd_config
 sudo sed -i 's/#PrintMotd yes/PrintMotd no/g' /etc/ssh/sshd_config
@@ -35,4 +48,10 @@ sudo rm /etc/motd
 
 echo "ufetch" | sudo tee /etc/zsh/zprofile
 
+echo "alias sup='sudo nala update'" | tee -a .zshrc
+echo "alias sin='sudo nala install'" | tee -a .zshrc
+echo "alias ls='exa -lh'" | tee -a .zshrc
+
 sudo systemctl restart sshd
+
+echo "Ejecuta ZSH para terminar la configuración"
