985744b87ec9231d906b04b20e23dc1c6d0c754d
Jaime Lopez <xhjl.jv@gmail.com>
2023-02-12 12:24:31 -0800

    Prueba docker

--- install.ps1
+++ install.ps1
@@ -349,3 +349,5 @@ Write-Host "Recuerda agregar a las ubicaciones de red el nas y poco x3" -Foregro
 #Update the windows terminal para que te deje poner los settings y dejarla bonita con winfetch al inicio en PWSH
 Write-Host "Update the windows terminal para que te deje poner los settings y dejarla bonita con winfetch al inicio en PWSH" -ForegroundColor Black -BackgroundColor White
 winget upgrade Microsoft.WindowsTerminal
+
+Write-Host "Prueba XD"sdsdfsdfsdf
