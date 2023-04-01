28194a28f841860e392219cb1f51a90e51401033
Jaime Lopez <xhjl.jv@gmail.com>
2023-04-01 11:31:58 -0700

    unrestricted ps1

--- install.ps1
+++ install.ps1
@@ -30,7 +30,7 @@ iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 
 # Se habilita la instalacion de scripts externos para instalar chocolatey
 Write-Host "Instalando Chocolatey" -ForegroundColor Black -BackgroundColor White
-Set-ExecutionPolicy AllSigned -Force
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco feature enable -n=useRememberedArgumentsForUpgrades
 
