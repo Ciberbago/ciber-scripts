3ec66e393da4a236644a822e339c93f3cdfb3e9c
Jaime Lopez <xhjl.jv@gmail.com>
2023-04-06 07:29:32 -0700

    Shorten the choco install script

--- install.ps1
+++ install.ps1
@@ -30,9 +30,7 @@ iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
 
 # Se habilita la instalacion de scripts externos para instalar chocolatey
 Write-Host "Instalando Chocolatey" -ForegroundColor Black -BackgroundColor White
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco feature enable -n=useRememberedArgumentsForUpgrades
+iex "& {$(irm https://community.chocolatey.org/install.ps1)}"
 
 #=================================================================================
 # Instalacion de programas con SCOOP
