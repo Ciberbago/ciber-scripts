ba5898cdff006ed8a5da1b0134ae8b8e585d8386
Jaime Lopez <xhjl.jv@gmail.com>
2023-07-18 13:23:34 -0700

    Added unattended upgrades and certain scoop progra

--- debian12.sh
+++ debian12.sh
@@ -2,7 +2,7 @@
 sudo apt update
 #Instalacion de paquetes
 sudo apt install -y nala
-sudo nala install -y bat curl duf exa fzf git htop lm-sensors lshw micro ncdu nload powertop radeontop rclone time wakeonlan zsh
+sudo nala install -y bat curl duf exa fzf git htop lm-sensors lshw micro ncdu nload powertop radeontop rclone time unattended-upgrades wakeonlan zsh
 #instalacion de ufetch
 sudo wget -O /usr/local/bin/ufetch https://raw.githubusercontent.com/Ciberbago/ciber-scripts/main/debian/ufetch.sh && sudo chmod +x /usr/local/bin/ufetch
 #Instalacion de gotop
@@ -75,6 +75,9 @@ echo "HISTSIZE=10000" | tee -a .zshrc
 echo "SAVEHIST=1000" | tee -a .zshrc
 echo "setopt SHARE_HISTORY " | tee -a .zshrc
 
+#Servicios
+sudo systemctl enable unattended-upgrades
+sudo systemctl start unattended-upgrades
 sudo systemctl restart sshd
 
 echo "Ejecuta ZSH para terminar la configuración"
