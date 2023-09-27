46fc6e783748f656f1ee563477da8eb167803956
Jaime Lopez <xhjl.jv@gmail.com>
2023-09-27 12:28:41 -0700

    Added bat, BT and gnome keyring

--- arch2.sh
+++ arch2.sh
@@ -1,8 +1,9 @@
 sudo sed -i '/\[multilib\]/,/Include/s/^#//' /etc/pacman.conf && sudo pacman -Syy
 #Paquetes normales
-sudo pacman -S xclip wget xdg-desktop-portal-gnome exa smbclient tailscale ntfs-3g transmission-gtk baobab gnome-characters gvfs gvfs-smb tilix handbrake ffmpegthumbnailer vlc tumbler file-roller thunar-archive-plugin gnome-calculator gnome-disk-utility geany less discord git kdiskmark blender micro gdm gnome-shell gnome-control-center gnome-tweaks flatpak timeshift ncdu thunar neofetch ffmpeg cargo qt6-base steam zsh --noconfirm
+sudo pacman -S bat gnome-keyring gnome-bluetooth-3.0 bluez bluez-utils xclip wget xdg-desktop-portal-gnome exa smbclient tailscale ntfs-3g transmission-gtk baobab gnome-characters gvfs gvfs-smb tilix handbrake ffmpegthumbnailer vlc tumbler file-roller thunar-archive-plugin gnome-calculator gnome-disk-utility geany less discord git kdiskmark blender micro gdm gnome-shell gnome-control-center gnome-tweaks flatpak timeshift ncdu thunar neofetch ffmpeg cargo qt6-base steam zsh --noconfirm
 #Servicio login
 sudo systemctl enable gdm.service
+sudo systemctl enable bluetooth.service
 #instalar paru
 sudo pacman -S --needed base-devel --noconfirm
 git clone https://aur.archlinux.org/paru.git
@@ -11,7 +12,7 @@ makepkg -si
 #Aur paquetes
 paru -S pacleaner insync appimagelauncher chrome-remote-desktop --noconfirm
 #flatpaks
-flatpak install flathub com.github.qarmin.czkawka net.fasterland.converseen com.heroicgameslauncher.hgl com.github.tchx84.Flatseal com.github.Matoking.protontricks com.mattjakeman.ExtensionManager com.obsproject.Studio org.prismlauncher.PrismLauncher org.gnome.Boxes org.kde.gwenview nz.mega.MEGAsync org.jdownloader.JDownloader org.kde.kget org.gnome.Connections com.microsoft.Edge org.gnome.font-viewer io.missioncenter.MissionCenter com.authy.Authy io.github.cboxdoerfer.FSearch -y
+flatpak install flathub dev.deedles.Trayscale com.github.qarmin.czkawka net.fasterland.converseen com.heroicgameslauncher.hgl com.github.tchx84.Flatseal com.github.Matoking.protontricks com.mattjakeman.ExtensionManager com.obsproject.Studio org.prismlauncher.PrismLauncher org.gnome.Boxes org.kde.gwenview nz.mega.MEGAsync org.jdownloader.JDownloader org.kde.kget org.gnome.Connections com.microsoft.Edge org.gnome.font-viewer io.missioncenter.MissionCenter com.authy.Authy io.github.cboxdoerfer.FSearch -y
 #Tweaks terminal
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
@@ -24,7 +25,7 @@ echo 'SELECTED_EDITOR="/usr/bin/micro"' | >> .selected_editor
 sudo chsh -s $(which zsh) $(whoami)
 #Aliases
 echo "alias buscar='history 1 | fzf'" | tee -a .zshrc
-echo "alias cat='batcat'" | tee -a .zshrc
+echo "alias cat='bat'" | tee -a .zshrc
 echo "alias cc='cd && clear'" | tee -a .zshrc
 echo "alias cheat='f() { curl cheat.sh/\$1; };f'" | tee -a .zshrc
 echo "alias espacio='sudo ncdu / --exclude=/media' " | tee -a .zshrc
@@ -47,3 +48,7 @@ echo "setopt SHARE_HISTORY " | tee -a .zshrc
 ####Set up a name and PIN
 ####Wait for successful output containing "Host ready to receive connections."
 ####Run crd --start
+
+####sudo tailscale set --operator=jaime
+
+### XCLICKER
