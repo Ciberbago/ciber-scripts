8b5ff808bcac2ee3bf521c28e41affd493fe23df
Jaime Lopez <xhjl.jv@gmail.com>
2023-11-03 15:41:17 -0700

    typo

--- arch.sh
+++ arch.sh
@@ -128,7 +128,7 @@ if [[ $1 == 'restore' ]]; then
   dconf reset -f '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/'
   dconf reset -f '/org/gnome/desktop/wm/keybindings/'
   dconf load '/org/gnome/desktop/wm/keybindings/' < gnome/keybindings.dconf
-  dconf load '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' < gnomep/custom-values.dconf
+  dconf load '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/' < gnome/custom-values.dconf
   dconf write '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings' "$(cat gnome/custom-keys.dconf)"
   echo "restore done"
   exit 0
