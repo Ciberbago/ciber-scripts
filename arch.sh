6fff1d24a88c144b45c6e917ec6f63381b50ddee
Jaime Lopez <xhjl.jv@gmail.com>
2024-01-31 22:38:30 -0800

    yay install no confirm

--- arch.sh
+++ arch.sh
@@ -61,7 +61,7 @@ sudo modprobe vboxdrv vboxnetadp vboxnetflt vboxpci
 #<-------instalar yay------->
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --noconfirm
 
 yay -S adw-gtk3-git authy blackbox-terminal blender-lts-bin clicker-git czkawka-gui-bin deadbeef fsearch gnome-extensions-cli headsetcontrol heroic-games-launcher-bin impression insync lite-xl-bin obs-cmd obs-studio-av1 pacleaner prismlauncher-qt5-bin qimgv resources speedtest++ steamtinkerlaunch-git video-trimmer --noconfirm
 
