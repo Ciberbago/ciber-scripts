775f92c24fb88f2e0a3486f4b72bdd8e6073209f
Jaime Lopez <xhjl.jv@gmail.com>
2024-02-10 10:06:43 -0800

    removed docker group add

--- dotfiles/wip.sh
+++ dotfiles/wip.sh
@@ -26,7 +26,6 @@ sudo wget -O /etc/ssh/sshd_config ${dotfiles}/sshd_config
 curl -fsSL https://tailscale.com/install.sh | sh
 #Instalacion docker
 curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
-sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 docker run hello-world
