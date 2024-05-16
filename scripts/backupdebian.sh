f8722ff73ba3521da14c0b28abb7eb6044d57c6e
Jaime Lopez <xhjl.jv@gmail.com>
2024-05-16 08:56:38 -0700

    Excluded folders for bak

--- scripts/backupdebian.sh
+++ scripts/backupdebian.sh
@@ -3,16 +3,13 @@ TOKEN="5591825953:AAH9HY6LxcuyoBZwNZGoAYNpe__LdtRxoPQ"
 ID="-724429088"
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
-cd /home/jaime
+cd /opt/yacht
 
-a=$(\time -p tar -czf backups/docker.tar.gz --exclude docker/qflood/config/ipc-socket --exclude=docker/jelly/cache --exclude=docker/jelly/data/metadata --exclude=docker/jelly/data/data/attachments --exclude=docker/jelly/data/data/subtitles --exclude=docker/komga/artemis/journal --exclude=docker/navi/cache docker 2>&1)
+a=$(\time -p tar -czf /home/jaime/docker.tar.gz --exclude=jelly/data/cache --exclude=jelly/data/data/metadata --exclude=jelly/data/data/attachments --exclude=jelly/data/data/subtitles --exclude=komga/data/artemis/journal --exclude=navi/data --exclude=torrent/data --exclude=code/data/data/code-server-ipc.sock --exclude=scrutiny --exclude=uptime  compose 2>&1)
 curl -s -X POST $URL -d chat_id=$ID -d text=" 📦 Tarball created 📦 %0A%0A""$a"
 
-b=$(rclone copy /home/jaime/backups google:rclone/docker -v 2>&1 | sed -ne '/Transferred:/,$ p')
+b=$(rclone copy /home/jaime/docker.tar.gz google:rclone/docker -v 2>&1 | sed -ne '/Transferred:/,$ p')
 curl -s -X POST $URL -d chat_id=$ID -d text=" ☁️  Tarball a GDrive ☁️ %0A%0A""$b"
 
 c=$(rclone copy /media/hdd/music google:Music -v 2>&1 | sed -ne '/Transferred:/,$ p')
 curl -s -X POST $URL -d chat_id=$ID -d text="🎸 Musica a GDrive 🎸 %0A%0A""$c"
-
-d=$(rclone move google:rclone/scan /home/jaime/docker/paper/docs/consume -v 2>&1 | sed -ne '/Transferred:/,$ p')
-curl -s -X POST $URL -d chat_id=$ID -d text="📷 Descargados docs 📷%0A%0A""$d"
