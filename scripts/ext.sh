333c38b7cd105bf30a0e3c7f539df70c75828820
Jaime Lopez <xhjl.jv@gmail.com>
2024-05-26 13:09:13 -0700

    Deleted weather and headset, added executor

--- scripts/ext.sh
+++ scripts/ext.sh
@@ -2,8 +2,7 @@ gext install 4269
 gext install 615
 gext install 3193
 gext install 1160
-gext install 5823
 gext install 5940
 gext install 2917
-gext install 6655
 gext install 6784
+gext install 2932
