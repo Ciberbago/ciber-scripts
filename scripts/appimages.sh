9994f589cd26e2c080775ab9722b58a7feac2e8d
Jaime Lopez <xhjl.jv@gmail.com>
2024-07-25 10:10:20 -0700

    Moved appimages to new script

---
+++ scripts/appimages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+am -i blender czkawka deadbeef-devel google-chrome heroic-games-launcher prismlauncher protonup-qt suyu ventoy
---
+++ scripts/postinstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+yay -S python-tqdm --noconfirm
+# Lista de scripts a ejecutar
+scripts=("ext.sh" "gnomeconfig.sh" "hideapps.sh" "removeapps.sh" "gnome.sh" "appimages.sh")
+
+# Directorio donde se encuentran los scripts
+directorio="$HOME"
+
+# Iteramos sobre la lista de scripts
+for script in "${scripts[@]}"; do
+    # Construimos la ruta completa del script
+    ruta="$directorio/$script"
+
+    # Verificamos si el archivo existe y es ejecutable
+    if [[ -x "$ruta" ]]; then
+        # Si es gnome.sh, ejecutamos con el argumento "restore"
+        if [[ "$script" == "gnome.sh" ]]; then
+            echo "Ejecutando $script con el argumento 'restore'"
+            "$ruta" restore
+        else
+            echo "Ejecutando $script"
+            "$ruta"
+        fi
+    else
+        echo "El script $ruta no existe o no tiene permisos de ejecución."
+    fi
+done
