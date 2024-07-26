d4e37ddd7296028275c4711f12a7e04f138820bf
Jaime Lopez <xhjl.jv@gmail.com>
2024-07-26 09:43:27 -0700

    Separated aur stuff installation

---
+++ scripts/aur.sh
@@ -0,0 +1,3 @@
+yay
+yay -S chaotic-aur/adw-gtk3-git clicker-git chaotic-aur/fsearch gnome-extensions-cli headsetcontrol chaotic-aur/insync obs-cmd chaotic-aur/resources webtorrent-mpv-hook --noconfirm
+
