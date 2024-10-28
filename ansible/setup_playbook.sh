5d07fbebd56365f5dfe20dbf74e35de2d8786993
Ciberbago <38386208+Ciberbago@users.noreply.github.com>
2024-10-28 10:33:02 -0700

    Update setup_playbook.sh

--- ansible/setup_playbook.sh
+++ ansible/setup_playbook.sh
@@ -3,7 +3,6 @@
 # Variables
 PLAYBOOK_PATH="$HOME/playbook.yml"
 INVENTORY_NAME="$HOME/hosts"
-SUDOERS_FILE="/etc/sudoers.d/ansible"
 
 # Comprobar si el script se está ejecutando con Bash
 if [ -z "$BASH_VERSION" ]; then
@@ -28,14 +27,6 @@ else
     echo "Ansible ya está instalado."
 fi
 
-# Configurar sudo para que no pida contraseña para el usuario actual
-USER=$(whoami)
-echo "Configurando sudo para no pedir contraseña..."
-if ! echo "$USER ALL=(ALL) NOPASSWD: ALL" | sudo tee $SUDOERS_FILE > /dev/null; then
-    echo "Error: No se pudo configurar sudo para no pedir contraseña."
-    exit 1
-fi
-
 # Crear el archivo de inventario
 echo "Creando el archivo de inventario..."
 if ! echo -e "[local]\nlocalhost ansible_connection=local" > $INVENTORY_NAME; then
@@ -52,12 +43,4 @@ if ! ansible-playbook -i $INVENTORY_NAME $PLAYBOOK_PATH; then
     exit 1
 fi
 
-# Revertir la configuración de sudo
-echo "Revirtiendo la configuración de sudo..."
-if ! sudo rm $SUDOERS_FILE; then
-    echo "Error: No se pudo revertir la configuración de sudo."
-    exit 1
-fi
-
-echo "Configuración de sudo revertida."
 echo "Proceso completo."
