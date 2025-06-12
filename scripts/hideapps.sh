71304d683b8f8d62795bfd7b3ed64af3c9247462
Jaime Lopez <xhjl.jv@gmail.com>
2025-06-12 14:43:37 -0700

    Added cmake to the hide list

--- scripts/hideapps.sh
+++ scripts/hideapps.sh
@@ -1,5 +1,5 @@
 sudo mkdir -p /usr/local/share/applications
 
-sudo cp /usr/share/applications/{nnn.desktop,btop.desktop,avahi-discover.desktop,assistant.desktop,designer.desktop,linguist.desktop,qdbusviewer.desktop,qv4l2.desktop,qvidcap.desktop,bvnc.desktop,bssh.desktop,fish.desktop,scrcpy-console.desktop,scrcpy.desktop} /usr/local/share/applications/
+sudo cp /usr/share/applications/{cmake-gui.desktop,nnn.desktop,btop.desktop,avahi-discover.desktop,assistant.desktop,designer.desktop,linguist.desktop,qdbusviewer.desktop,qv4l2.desktop,qvidcap.desktop,bvnc.desktop,bssh.desktop,fish.desktop,scrcpy-console.desktop,scrcpy.desktop} /usr/local/share/applications/
 
 sudo sed -i '$ a Hidden=true' /usr/local/share/applications/*
