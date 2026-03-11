b7669e7020c9b85241fa5c005bee98e387596ea2
Jaime Lopez <xhjl.jv@gmail.com>
2026-03-11 09:11:18 -0700

    Added packages, changed firefox and mpv

--- arch.sh
+++ arch.sh
@@ -18,7 +18,7 @@ sudo sed -i "/etc/pacman.conf" \
 #<-------Ajustes para compilacion-------->#
 sudo sed -i "/etc/makepkg.conf" \
     -e "s|^#BUILDDIR=.*|&\nBUILDDIR=/tmp/makepkg|" \
-    -e "s|^PKGEXT.*|PKGEXT='.pkg.tar'|" \
+    -e "s|^PKGEXT.*|PKºGEXT='.pkg.tar'|" \
     -e "s|^OPTIONS=.*|#&\nOPTIONS=(docs \!strip \!libtool \!staticlibs emptydirs zipman purge \!debug lto)|" \
     -e "s|-march=.* -mtune=generic|-march=native|" \
     -e "s|^#RUSTFLAGS=.*|&\nRUSTFLAGS=\"-C opt-level=2 -C target-cpu=native\"|" \
@@ -28,7 +28,7 @@ sudo pacman -Syy
 declare -a pkgs pkgs_200 pkgs_202 pkgs_404
 declare -A pkgs_301
 
-pkgs=(7zip adw-gtk-theme android-tools baobab base-devel bat bluez bluez-utils btop dkms ethtool eza fastfetch ffmpegthumbnailer file-roller firefox fish fisher flameshot fragments freerdp fzf gdm gdu git gnome-bluetooth-3.0 gnome-calculator gnome-characters gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-remote-desktop gnome-shell gnome-tweaks gnome-text-editor gvfs gvfs-smb handbrake imagemagick jre8-openjdk jre17-openjdk jre21-openjdk jq iperf3 less libmad linux-headers linux-lts loupe mangohud mesa micro minizip mpv-mpris nautilus net-tools nnn noto-fonts-cjk ntfs-3g obs-studio papirus-icon-theme pacman-contrib pkgfile python-tqdm qt5ct qt6-base radeontop reflector remmina resources rocm-smi-lib rust scrcpy smbclient steam tailscale terminator traceroute ttf-firacode-nerd tumbler uget unrar usbutils virtualbox virtualbox-guest-iso vulkan-radeon webp-pixbuf-loader wget wl-clipboard xdg-desktop-portal-gnome yt-dlp)
+pkgs=(7zip adw-gtk-theme android-tools baobab base-devel bat bluez bluez-utils btop dkms ethtool eza fastfetch ffmpegthumbnailer file-roller firefox fish fisher flameshot fragments freerdp fzf gdm gdu git gnome-bluetooth-3.0 gnome-calculator gnome-characters gnome-control-center gnome-disk-utility gnome-font-viewer gnome-keyring gnome-remote-desktop gnome-shell gnome-tweaks gnome-text-editor gvfs gvfs-smb handbrake imagemagick jre8-openjdk jre17-openjdk jre21-openjdk jq iperf3 less libmad linux-headers linux-lts loupe mangohud mesa minizip mpv-mpris nautilus net-tools nnn noto-fonts-cjk ntfs-3g obs-studio papirus-icon-theme pacman-contrib papers pkgfile python-tqdm qt5ct qt6-base radeontop reflector remmina resources rocm-smi-lib rust scrcpy smbclient steam tailscale terminator traceroute ttf-firacode-nerd tumbler uget unrar usbutils virtualbox virtualbox-guest-iso vulkan-radeon webp-pixbuf-loader wget wl-clipboard xdg-desktop-portal-gnome)
 
 pkgs=($(printf '%s\n' "${pkgs[@]}"|sort -u))
 pkgs_200=($(comm -12 <(pacman -Slq|sort -u) <(printf '%s\n' "${pkgs[@]}")))
@@ -52,8 +52,10 @@ mkdir -p ~/.config/autostart
 mkdir -p ~/.config/fish
 mkdir -p ~/.config/mpv/fonts
 mkdir -p ~/.config/mpv/scripts
+mkdir -p ~/.config/mpv/script-opts
 mkdir -p ~/.config/obs-studio/basic/profiles/Untitled/
 mkdir -p ~/.config/yay
+mkdir -p ~/.config/terminator
 mkdir -p gnome
 mkdir -p Screenshots/tmp
 mkdir -p ~/.local/share/nautilus/scripts
@@ -74,6 +76,7 @@ wget -O ~/.config/fish/config.fish ${dotfiles}/config.fish
 wget -O ~/.config/mpv/mpv.conf ${dotfiles}/mpv.conf
 wget -O ~/.config/mpv/scripts/modern.lua ${dotfiles}/modern.lua
 wget -O ~/.config/mpv/scripts/thumbfast.lua ${dotfiles}/thumbfast.lua
+wget -O ~/.config/mpv/script-opts/osc.conf ${dotfiles}/osc.conf
 wget -O ~/.config/mpv/fonts/Material-Design-Iconic-Font.ttf ${dotfiles}/Material-Design-Iconic-Font.ttf 
 wget -O ~/.config/obs-studio/basic/profiles/Untitled/basic.ini ${dotfiles}/obsprofile.ini
 wget -O ~/.config/obs-studio/basic/profiles/Untitled/recordEncoder.json ${dotfiles}/obsrecorder.json
--- scripts/firefoxconfig.sh
+++ scripts/firefoxconfig.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
-sudo rm -r ~/.mozilla
+# juda configuración vieja, sea donde sea que esté
+rm -rf "$HOME/.mozilla" "$HOME/.config/mozilla"
 
-#Creates and generates default profile
-nohup firefox --headless --CreateProfile jaime &
+# ruta donde se crea el perfil por defecto
+# (Firefox siempre genera ~/.mozilla/firefox, pero no está de más
+# comprobar)
+BASE="$HOME/.mozilla/firefox"
+mkdir -p "$BASE"
 
-#Gives it time to generate
-sleep 5
+# crear el perfil “jaime” en un subdirectorio controlado
+PROF_BASE="$BASE/jaime"
+firefox --CreateProfile "jaime $PROF_BASE" >/dev/null 2>&1
+sleep 1   # suele no ser necesario, sólo para asegurarse
 
-#Gets profile's path
-cd ~/.mozilla/firefox/
-if [[ $(grep '\[Profile[^0]\]' profiles.ini) ]]
-then PROFPATH=$(grep -E '^\[Profile|^Path|^Default' profiles.ini | grep -1 '^Default=1' | grep '^Path' | cut -c6-)
-else PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
+# ahora analizamos profiles.ini en la carpeta que corresponda
+# (el archivo puede estar en ~/.mozilla/firefox o ~/.config/mozilla/firefox,
+# tomamos el primero que exista).
+if [[ -f "$HOME/.mozilla/firefox/profiles.ini" ]]; then
+    INIFILE="$HOME/.mozilla/firefox/profiles.ini"
+elif [[ -f "$HOME/.config/mozilla/firefox/profiles.ini" ]]; then
+    INIFILE="$HOME/.config/mozilla/firefox/profiles.ini"
+else
+    echo "profiles.ini not found" >&2
+    exit 1
 fi
 
-#Copies config file to firefox profile
-cp ~/.config/firefoxuser.js ~/.mozilla/firefox/$PROFPATH/user.js
+cd "$(dirname "$INIFILE")" || exit 1
+if grep -q '\[Profile[^0]\]' profiles.ini; then
+    PROFPATH=$(grep -E '^\[Profile|^Path|^Default' profiles.ini \
+               | grep -1 '^Default=1' \
+               | grep '^Path' | cut -c6-)
+else
+    PROFPATH=$(grep 'Path=' profiles.ini | sed 's/^Path=//')
+fi
 
-#Creates folder for theme
-mkdir -p ~/.mozilla/firefox/$PROFPATH/chrome
+# el resto no cambia
+cp "$HOME/.config/firefoxuser.js" "$PROFPATH/user.js"
+mkdir -p "$PROFPATH/chrome"
+echo '@import "onebar/onebar.css";' > "$PROFPATH/chrome/userChrome.css"
+git clone https://git.gay/Freeplay/firefox-onebar.git \
+      "$PROFPATH/chrome/onebar"
 
-#Creates source file for importing themes
-echo '@import "onebar/onebar.css";' > ~/.mozilla/firefox/$PROFPATH/chrome/userChrome.css
-
-#Installs theme
-git clone https://git.gay/Freeplay/firefox-onebar.git ~/.mozilla/firefox/$PROFPATH/chrome/onebar
-
-echo -e "\n[Install4F96D1932A9F858E]\nDefault=$PROFPATH\nLocked=1" >> ~/.mozilla/firefox/profiles.ini
+echo -e "\n[Install4F96D1932A9F858E]\nDefault=$PROFPATH\nLocked=1" \
+     >> profiles.ini
